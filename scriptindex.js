e9b47905892b36d1b467790f708f1d93fe3a729c
Smart Coder <69685153+shiva-6214@users.noreply.github.com>
2021-03-16 18:49:39 +0530

    Add files via upload

---
+++ scriptindex.js
@@ -0,0 +1,33 @@
+function toggle() {
+	document.querySelector(".navbar").classList.toggle("navbar--open");
+}
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+window.addEventListener('load', () => {
+
+    // Via Query parameters - GET
+    /* const params = (new URL(document.location)).searchParams;
+    const name = params.get('name');
+    const surname = params.get('surname'); */
+
+    // Via local Storage
+    /* const name = localStorage.getItem('NAME');
+    const surname = localStorage.getItem('SURNAME'); */
+    
+    const name = sessionStorage.getItem('Correcthaina');
+    
+    /*document.getElementById('result-name').innerHTML = name;
+    document.getElementById('result-surname').innerHTML = surname;*/
+
+    if (name == "sahipage") {
+        console.log("verified!");
+    }
+
+    else {
+        window.location.href = "https://sowmya.sarovar.ml";
+    }
+
+})
